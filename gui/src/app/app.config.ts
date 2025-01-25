21c83b1b5a86fa8dfa9cf011f5259547483ed0db
Raffaele <esinware_specia@camera.it>
2025-01-25 13:12:25 +0100

    Style fixed

--- gui/src/app/app.config.ts
+++ gui/src/app/app.config.ts
@@ -3,6 +3,9 @@ import { provideRouter, Routes } from "@angular/router";
 import { provideHttpClient, withInterceptors } from "@angular/common/http";
 import { backEndInterceptor } from "./extension/back-end-interceptor";
 import { TextComponent } from "./components/text.component";
+import { providePrimeNG } from "primeng/config";
+import Nora from "@primeng/themes/nora";
+import { definePreset } from "@primeng/themes";
 
 const routes: Routes = [
 	{ path: '', component: TextComponent },
@@ -10,9 +13,35 @@ const routes: Routes = [
 	{ path: '**', redirectTo: '' }
 ];
 
+const customStyle = definePreset(Nora, {
+    semantic: {
+        primary: {
+            50: '{indigo.50}',
+            100: '{indigo.100}',
+            200: '{indigo.200}',
+            300: '{indigo.300}',
+            400: '{indigo.400}',
+            500: '{indigo.500}',
+            600: '{indigo.800}',
+            700: '{indigo.700}',
+            800: '{indigo.800}',
+            900: '{indigo.900}',
+            950: '{indigo.950}'
+        }
+    }
+});
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withInterceptors([backEndInterceptor]))
+    provideHttpClient(withInterceptors([backEndInterceptor])),	
+	providePrimeNG({
+		theme: {
+			preset: customStyle,
+			options: {
+				darkModeSelector: '.my-app-dark'
+			}
+		}
+	})
   ]
 };
