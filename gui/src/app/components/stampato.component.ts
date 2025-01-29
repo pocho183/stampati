bd3fbe1add4892c4856b10524dd942975986f363
pocho183 <manuel.corini@gmail.com>
2025-01-29 21:56:33 +0100

    Added splitter

--- gui/src/app/components/stampato.component.ts
+++ gui/src/app/components/stampato.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { SplitterModule } from 'primeng/splitter';
 import { RicercaComponent } from './ricerca/ricerca.component';
 import { BarcodeComponent } from './barcode/barcode.component';
 import { FrontespizioComponent } from "./frontespizio/frontespizio.component";
@@ -8,7 +9,8 @@ import { PresentazioneComponent } from "./presentazione/presentazione.component"
 @Component({
 	standalone: true,
 	selector: 'stampato',
-	imports: [RicercaComponent, BarcodeComponent, FrontespizioComponent, RelatoriComponent, PresentazioneComponent],
+	imports: [SplitterModule, RicercaComponent, BarcodeComponent, FrontespizioComponent, RelatoriComponent, 
+		PresentazioneComponent],
 	templateUrl: './stampato.component.html',
 	styleUrl: './stampato.component.css'
 })
